bce85ad2efaf72319c2c0cca862b80a5e2d68c04
Sergey Shishkin <shishkinsa1997@gmail.com>
2026-03-10 03:41:04 +0700

    feat: complete l03

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-const num = 266219
+// 1
+const lang = prompt("Enter language", "en");
+const ru = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// a
+if (lang === "en") {
+  console.log(en);
+} else if (lang === "ru") {
+  console.log(ru);
+} else {
+  console.log("Error");
+}
+// b
+switch (lang) {
+  case "en":
+    console.log(en);
+    break;
+  case "ru":
+    console.log(ru);
+    break;
+  default:
+    console.log("Error");
+    break;
+}
+// c
+const langs = { en, ru };
+lang === "en"
+  ? console.log(langs.en)
+  : lang === "ru"
+    ? console.log(langs.ru)
+    : console.log("Error");
 
-let sum = (num.toString().split("")).reduce((x, acc) => {
-  return Number(x) * acc
-});
-sum = sum ** 2
-const len = sum.toString().length
-sum = Math.floor(sum / 10 ** (len - 2))
-
-console.log(sum);
+// 2
+const namePerson = prompt("Enter name", "Artem");
+console.log(
+  namePerson === "Artem"
+    ? "director"
+    : namePerson === "Alexander"
+      ? "teacher"
+      : "student",
+);
